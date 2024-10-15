0dbbfd90db1cc85ef1181843ecbd92f099c96728
ALEXANDRE.CLASQUIN <alexandre.clasquin@gmail.com>
2024-10-15 11:22:22 +0200

    test node

---
+++ index.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+
+"Ludo";
+console.log(chalk.black("Ludo"));
+"Simon";
+console.log(chalk.red("Simon"));
+"Badre";
+console.log(chalk.green("Badre"));
+"Clément";
+console.log(chalk.yellow("Clément"));
